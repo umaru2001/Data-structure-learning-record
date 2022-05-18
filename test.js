beff59e3990fc7360734924348524d1c213241d6
U.M.R <55972955+umaru2001@users.noreply.github.com>
2022-05-18 09:21:40 +0800

    initial commit
    test test

--- test.js
+++ test.js
@@ -1,1 +1,15 @@
-console.log("hello, world!");
+console.log('hello, world!');
+
+function quickSort(nums) {
+  if (nums.length === 0) return [];
+  let pivot = nums[0];
+  let left = [], right = [];
+  for (let i = 1; i < nums.length; i++) {
+    const element = nums[i];
+    if (element > pivot) right.push(element);
+    else left.push(element);
+  }
+  return quickSort(left).concat([pivot], quickSort(right));
+}
+
+console.log(quickSort([10, 3, 4, 2, 7, 5]));
